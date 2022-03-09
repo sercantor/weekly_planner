773c83c784a1e200b13f2a0726a0ca9b6de25fc1
sercan tor <tor.sercan@gmail.com>
2022-03-09 21:29:59 +0300

    add: task size to be input

--- week_planner_generator.py
+++ week_planner_generator.py
@@ -11,10 +11,12 @@ print("""1: 'Monday'\n2: 'Tuesday'\n3: 'Wednesday'\n4: 'Thursday'\n5: 'Friday'\n
 days = int(input('How many weeks?\n'))
 start_day = int(input('What is your start day? Monday is 1, Sunday is 7\n'))
 end_day = int(input('What is your end day? Monday is 1, Sunday is 7\n'))
+todo_number = int(input('How many tasks on average do you need for a day?\n'))
 
 start_day_value = d[start_day]
 end_day_value = d[end_day]
 timestr = time.strftime("%d-%b-%Y")
+todos = todo_number *  "- [ ]\n"
 
 content = """"""
 file = open("{}.md".format(timestr), "w+")
@@ -25,25 +27,16 @@ for i in range(days*7 -2):
 		if d[end_day + 1] in timediff or d[end_day + 2] in timediff:
 			continue
 		content += """## {}
-- [ ] 
-- [ ] 
-- [ ] 
-- [ ] 
-- [ ] 
-""".format(timediff)
+{}
+""".format(timediff, todos)
 	else:	
 		if d[end_day + 1] in timediff or d[end_day + 2] in timediff:
 			continue
 		content += """
 ## {}
-- [ ] 
-- [ ] 
-- [ ] 
-- [ ] 
-- [ ] 
-
+{}
 ---
-""".format(timediff)
+""".format(timediff, todos)
 	
 
 file.write(content)
